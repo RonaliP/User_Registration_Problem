47040ceb4f81e6443cec11c603f660f817765af4
Ronali <ronalipanigrahy88@gmail.com>
2020-10-21 23:55:19 +0530

    THE CODE TO VALIDATE FULL NAME ADDED

--- User.sh
+++ User.sh
@@ -1,14 +1,22 @@
+#!/bin/bash
+
 echo "THIS PROGRAM WILL CONTAIN CODES FOR VALIDATING EACH ENTRY OF USER REGISTRATION"
 
 shopt -s extglob
 
-Firstname_regex="^[A-Z]{1}[a-zA-Z]{2,}$"
+regex="^([A-Z]{1}[a-zA-Z]{2,})+\s+([A-Z]{1}[a-zA-Z]{2,})+$"
 
-read -p "ENTER USER_FIRST_NAME=" name
 
-if [[ $name =~ $Firstname_regex ]]
+read -p "ENTER NAME=" name
+
+if [[ $name =~ $regex ]]
 then
-	echo "VALID"
+	echo "VALID NAME"
 else
 	echo "INVALID INPUT"
 fi
+
+
+
+
+
