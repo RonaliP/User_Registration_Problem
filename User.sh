946a910947114ca0af26bc0529d565eda7262e17
Ronali <ronalipanigrahy88@gmail.com>
2020-10-21 22:23:35 +0530

    First name validation added

--- User.sh
+++ User.sh
@@ -1,2 +1,14 @@
 echo "THIS PROGRAM WILL CONTAIN CODES FOR VALIDATING EACH ENTRY OF USER REGISTRATION"
 
+shopt -s extglob
+
+Firstname_regex="^[A-Z]{1}[a-zA-Z]{2,}$"
+
+read -p "ENTER USER_FIRST_NAME=" name
+
+if [[ $name =~ $Firstname_regex ]]
+then
+	echo "VALID"
+else
+	echo "INVALID INPUT"
+fi
